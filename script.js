c90fe17d950ff626676a15dea9dd8dd045fc474b
suyunovdev <153995037+suyunovdev@users.noreply.github.com>
2024-12-18 18:03:04 +0500

    years and months adedd

--- script.js
+++ script.js
@@ -2,15 +2,21 @@ let hours = document.getElementById("hours");
 let mins = document.getElementById("mins");
 let secs = document.getElementById("secs");
 let days = document.getElementById("days");
+let years = document.getElementById("years");
+let months = document.getElementById("months");
 
 setInterval(() => {
   let date = new Date();
+  let year = date.getFullYear();
+  let month = date.getMonth();
   let dd = date.getDate();
   let hh = date.getHours();
   let mm = date.getMinutes();
   let ss = date.getSeconds();
 
   days.innerHTML = dd;
+  years.innerHTML = year;
+  months.innerHTML = month;
   hours.innerHTML = hh;
   mins.innerHTML = mm;
   secs.innerHTML = ss;
